4a2e505744918e44f69f2d78d73ba989c4b62ecc
registermail@qq.com <Github15>
2020-01-31 02:47:08 +0800

    根据腾讯要求，修改了程序。

--- miniprogram/pages/select/select.js
+++ miniprogram/pages/select/select.js
@@ -1,7 +1,130 @@
 Page({
+  compare: function (property) {
+    return function (a, b) {
+      var val_1 = Date.parse(a[property]);
+      var val_2 = Date.parse(b[property]);
+      return val_2 - val_1;
+    }
+  },
   data: {
     noVal: '',
     dateVal: ''
+  },
+  onShow: function (){
+    wx.showLoading({
+      title: '首次加载较慢'
+    });
+    this.setData({
+      result: []
+    })
+    if (this.data.noVal == null) {
+      this.data.noVal = ''
+    }
+    if (this.data.dateVal == null) {
+      this.data.dateVal = ''
+    }
+    //
+    // 查询
+    //
+    // 1 如果日期和车次都不是 null
+    const db = wx.cloud.database()
+    if (this.data.dateVal != '' && this.data.noVal != '') {
+      wx.cloud.callFunction({
+        name: 'getSelectWithDateWithNo',
+        data: {
+          noVal: this.data.noVal,
+          dateVal: this.data.dateVal
+        },
+        success: res => {
+          this.setData({
+            result: res.result.data
+          })
+          setTimeout(function () {
+            wx.hideLoading();
+          }, 5000)
+        },
+        fail: err => {
+          this.setData({
+            result: null
+          })
+          setTimeout(function () {
+            wx.hideLoading();
+          }, 5000)
+        }
+      })
+    }
+    // 2 如果日期不是 '' 车次是 ''
+    if (this.data.dateVal != '' && this.data.noVal == '') {
+      wx.cloud.callFunction({
+        name: 'getSelectWithDateWithoutNo',
+        data: {
+          dateVal: this.data.dateVal
+        },
+        success: res => {
+          this.setData({
+            result: res.result.data
+          })
+          setTimeout(function () {
+            wx.hideLoading();
+          }, 5000)
+        },
+        fail: err => {
+          this.setData({
+            result: null
+          })
+          setTimeout(function () {
+            wx.hideLoading();
+          }, 5000)
+        }
+      })
+    }
+    // 3 如果日期是 '' 车次不是 ''
+    if (this.data.dateVal == '' && this.data.noVal != '') {
+      wx.cloud.callFunction({
+        name: 'getSelectWithoutDateWithNo',
+        data: {
+          noVal: this.data.noVal
+        },
+        success: res => {
+          this.setData({
+            result: res.result.data
+          })
+          setTimeout(function () {
+            wx.hideLoading();
+          }, 5000)
+        },
+        fail: err => {
+          this.setData({
+            result: null
+          })
+          setTimeout(function () {
+            wx.hideLoading();
+          }, 5000)
+        }
+      })
+    }
+    // 4 如果日期是 ''  车次是 ''
+    if (this.data.dateVal == '' && this.data.noVal == '') {
+      wx.cloud.callFunction({
+        name: 'getAll',
+        success: res => {
+          this.setData({
+            result: res.result.data
+          })
+          setTimeout(function () {
+            wx.hideLoading();
+          }, 5000)
+        },
+        fail: err => {
+          this.setData({
+            result: null
+          })
+          setTimeout(function () {
+            wx.hideLoading();
+          }, 5000)
+        }
+      })
+    }
   },
   copyEmail() {
     wx.setClipboardData({
@@ -104,11 +227,23 @@ Page({
     }
     // 4 如果日期是 ''  车次是 ''
     if (this.data.dateVal == '' && this.data.noVal == '') {
-      this.setData({
-        result: null
+      wx.cloud.callFunction({
+        name: 'getAll',
+        data: {
+          noVal: this.data.noVal
+        },
+        success: res => {
+          this.setData({
+            result: res.result.data
+          })
+        },
+        fail: err => {
+          this.setData({
+            result: null
+          })
+        }
       })
     }
-
   },
   inputChange(e){
     this.data.noVal = e.detail.value
@@ -131,7 +266,6 @@ Page({
           dateVal: this.data.dateVal
         },
         success: res => {
-          console.log(res.result.data)
           this.setData({
             result: res.result.data
           })
@@ -183,8 +317,21 @@ Page({
     }
     // 4 如果日期是 ''  车次是 ''
     if (this.data.dateVal == '' && this.data.noVal == '') {
-      this.setData({
-        result: null
+      wx.cloud.callFunction({
+        name: 'getAll',
+        data: {
+          noVal: this.data.noVal
+        },
+        success: res => {
+          this.setData({
+            result: res.result.data
+          })
+        },
+        fail: err => {
+          this.setData({
+            result: null
+          })
+        }
       })
     }
   }
